b3ae12c30689922e4252d29ff1089b4da83359d2
Unic-X <armansinghkshatri69@gmail.com>
2024-03-31 12:25:21 +0530

    Hamburger menu added

--- assets/js/head.js
+++ assets/js/head.js
@@ -51,6 +51,12 @@ async function copyCodeToClipboard(button, highlightDiv) {
     highlightDiv.removeChild(textArea);
   }
   
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navbarLinks = document.getElementsByClassName('nav__list')[0]
+  
+  toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+  })
 
   console.log(
     `%c
